c7169d92cbefa4e3f2d71ffd01b1aaced5631c02
yuliac <6183yc@gmail.com>
2020-08-14 20:37:30 -0500

    changes

--- scriptapp.js
+++ scriptapp.js
@@ -7,7 +7,7 @@
 // create function populate questions
 function populate() {
     if (quiz.isEnded()) {
-        // showScores()
+        showScores()
     }
     else {
         // show questions
@@ -39,14 +39,20 @@ function showProgress() {
     var element = document.getElementById("progress");
     element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
 };
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
 
 // set the questions:
 
 var questions = [
-    new Question("Students that have a lower tolerance for ambiguity may be uncomfortable not understanding everything they see and hear and may be more anxious about their performance in the classroom.’ What is the most appropriate strategy to handle this?", ["Repeat multiple times the same phrase in the target language", "Use full-phrase translation to help the student understand in English", "Provide comprehensible input", "Do nothing"], "Provide comprehensible input"),
+    new Question("Sometimes students have a lower tolerance for ambiguity. What is the most appropriate strategy to handle this?", ["Repeat multiple times the same phrase in the target language", "Use full-phrase translation to help the student understand in English", "Provide comprehensible input", "Do nothing"], "Provide comprehensible input"),
     new Question("This is a type of comprehension check that helps students develop vocabulary", ['Bookending', 'Binary option', 'Venn diagram', 'Text alerts'], "Binary option"),
-    new Question("Describing or defining a concept instead of saying or writing the specific words, usually in order to fill in a lexical gap.’ What strategy does this refer to?", ['Cognates', 'Circumlocution', 'Glossing', 'Discussion'], 'Circumlocution'),
-    new Question('How long is an appropriate time for small talk in class?', ['20 min ', '5 min;', 'As long as it takes', 'No small talk in class'], '5 min'),
+    new Question("Describing or defining a concept instead of saying or writing the specific words, usually in order to fill in a lexical gap. What strategy does this refer to?", ['Cognates', 'Circumlocution', 'Glossing', 'Discussion'], 'Circumlocution'),
+    new Question('How long is an appropriate time for small talk in class?', ['20 min ', '5 min', 'As long as it takes', 'No small talk in class'], '5 min'),
     new Question('What does it mean to modify your speech for students?', ["translate for students", "speak slower", "reduce your rate of speech", "speak louder"], "reduce your rate of speech")
 ];
 
--- scriptquizcontroller.js
+++ scriptquizcontroller.js
@@ -1,25 +1,7 @@
 
 
 // this page is for scores and  number of questions
-// function Quiz(questions) {
-//     this.score = 0;
-//     this.questions = questions;
-//     this.questionIndex = 0;
-// }
-// Quiz.prototype.getQuestionIndex = function () {
-//     return this.questions[this.questionIndex];
 
-// }
-// Quiz.prototype.guess = function (answer) {
-//     if (this.getQuestionIndex().isCorrectAnswer(answer)) {
-//         this.score++;
-//     }
-
-//     this.questionIndex++;
-// }
-// Quiz.prototype.isEnded = function () {
-//     return this.questionIndex === this.questions.length;
-// }
 
 function Quiz(questions) {
     this.score = 0;
